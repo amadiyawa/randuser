eec1d947aeaca4f0b13d05529b3184a71d1ac65a
Amadou Iyawa <amadiyawa@yahoo.com>
2024-04-18 17:43:13 +0100

    fix app name

--- app/src/main/java/com/amadiyawa/ennovtestapp/RandusersApplication.kt
+++ app/src/main/java/com/amadiyawa/ennovtestapp/RandusersApplication.kt
@@ -1,15 +1,13 @@
 package com.amadiyawa.ennovtestapp
 
 import android.app.Application
-import android.util.Log
 import com.amadiyawa.feature_users.featureUserModule
-import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.GlobalContext
 import timber.log.Timber
 
-class EnnovTestAppApplication: Application() {
+class RandusersApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
@@ -20,12 +18,10 @@ class EnnovTestAppApplication: Application() {
     private fun initKoin() {
         GlobalContext.startKoin {
             androidLogger()
-            androidContext(this@EnnovTestAppApplication)
+            androidContext(this@RandusersApplication)
 
             modules(appModule)
-            Log.d("appModule","${appModule.isLoaded} created")
             modules(featureUserModule)
-            Log.d("featureUserModule","${featureUserModule.first().isLoaded} created")
         }
     }
 
